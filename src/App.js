ddb3f4236274747cd8c87c7d849b94c91a36716b
nschuler <nicholas.schuler@student.uts.edu.au>
2020-12-08 23:25:53 +1100

    Basic Styling with GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Home from './pages/Home';
+import GlobalStyles from './components/GlobalStyles';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello Ignite!</h1>
+      <GlobalStyles />
       <Home />
     </div>
   );
